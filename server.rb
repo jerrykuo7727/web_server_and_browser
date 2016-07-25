adedd10e0122919f47d5083b082a5b05098e1b50
Jiazhi Guo <jerrykuo7727@gmail.com>
2016-07-25 17:31:56 +0800

    Modify server.rb to parse HTTP GET request

--- server.rb
+++ server.rb
@@ -3,7 +3,37 @@ require 'socket'
 server = TCPServer.open(2000)
 loop {
   client = server.accept
-  client.puts(Time.now.ctime)
-  client.puts "Closing the connection. Bye!"
+
+  request = client.gets.scan(/^(\S+) (\S+) (\S+)/).flatten
+  method = request[0]
+  filename = request[1][1..-1] 
+  protocol = request[2]
+
+  case method
+  when 'GET'
+    file_exists = File.exist?(filename)
+
+    if file_exists
+      client.puts "#{protocol} 200 OK"
+      client.puts("Date: #{Time.now.ctime}")
+
+      content_type = filename.scan(/\w+$/)[0]
+      client.puts "Content-Type: #{content_type}"
+
+      content = File.read(filename)
+      client.puts "Content-Length: #{content.size}"
+      client.puts
+      client.puts content
+
+    else
+      client.puts "#{protocol} 404 Not Found"
+      client.puts("Date: #{Time.now.ctime}")
+    end
+
+  else
+    client.puts "#{protocol} 500 Unknown Method"
+    client.puts("Date: #{Time.now.ctime}")
+  end
+
   client.close
 }
--- web_browser.rb
+++ web_browser.rb
@@ -1,13 +1,12 @@
 require 'socket'
 
-host = 'www.tutorialspoint.com'
-port = 80
-path = "/index.htm"
+host = 'localhost'
+port = 2000
+path = "/index.html"
 
 request = "GET #{path} HTTP/1.0\r\n\r\n"
 
 socket = TCPSocket.open(host, port)
-socket.print(request)
+socket.puts(request)
 response = socket.read
-headers, body = response.split("\r\n\r\n", 2)
-print body
+puts response
