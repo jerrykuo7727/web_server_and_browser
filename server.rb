ad6b59273a6a37dc2cafd7d7f6c695613bb423f6
Jiazhi Guo <jerrykuo7727@gmail.com>
2016-07-25 15:47:56 +0800

    Add client.rb

--- server.rb
+++ server.rb
@@ -1,9 +1,9 @@
-require 'socket'               # Get sockets from stdlib
+require 'socket'
 
-server = TCPServer.open(2000)  # Socket to listen on port 2000
-loop {                         # Servers run forever
-  client = server.accept       # Wait for a client to connect
-  client.puts(Time.now.ctime)  # Send the time to the client
+server = TCPServer.open(2000)
+loop {
+  client = server.accept
+  client.puts(Time.now.ctime)
   client.puts "Closing the connection. Bye!"
-  client.close                 # Disconnect from the client
+  client.close
 }
